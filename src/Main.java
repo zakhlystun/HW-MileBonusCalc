faa82a51435741079646bfba9a7c1c85049f6e7a
Sergey Zakhlystun <zakhlystun@gmail.com>
2023-12-06 14:40:13 +0300

    Bonus calculating added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        int ticketPrice = 27550;
+        int mileSize = 20;
+
+        int bonusSize = ticketPrice / mileSize;
+
+        System.out.println("Ваш бонус составляет " + bonusSize + " миль");
+
     }
 }
